69a02d814717e826692b0efbfebbaac0aac9f929
muhammet-ozkaraca <muhammetozk@icloud.com>
2022-03-16 14:29:58 +0300

    Week1 Assignment-Completed

--- Week-1/Assignment/Week1-Assignment.R
+++ Week-1/Assignment/Week1-Assignment.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(ggplot2)
 library(lubridate)
 
+
+setwd("/Users/muhammetozkaraca/Desktop/ESOC-Replication/Week-1/Assignment/Week1_csvfiles")
+
 data <- list.files(pattern="*.csv")
 dataset <- lapply(data, read.csv)
 
@@ -86,6 +89,50 @@ data_final %>%
 data_final %>%
   filter(Public == 1) %>%
   count()
+
+# Write a function that calculates descriptive statistics (N, mean, min, median, max, and S.D.). 
+# Then, loop over each of the three tweet count variables in the data to generate those descriptives and
+# output a 3 x 7 dataframe of these with the variable name.
+
+
+my.summary <- function(x,...){
+  c(N=length(x),
+    mean=mean(x, ...),
+    min=min(x, ...),
+    median=median(x, ...),
+    max=max(x,...), 
+    S.D. = sd(x,...))
+}
+
+summary_df <- as.data.frame(apply(data_final[2:4], 2, my.summary)) # note that the second argument is called margin and denotes whether the manipulation is performed on rows or columns. For rows, it is 1, and for columns, it is 2.
+summary_df
+
+
+# Try different plots to visualize the distribution of each tweet count variable. 
+# Which type of graph is most effective in your opinion, and why?
+
+
+tweets <- data_final[, 1:4] %>%
+  pivot_longer(tweet_count_all:tweet_count_blm, names_to = "Tweets", values_to = "Count")
+
+ggplot(tweets, aes(x = Tweets, y = Count, color = Tweets)) + 
+  geom_boxplot() +
+  coord_flip() +
+  theme_minimal()
+
+
+# What do you notice about the distribution of these variables? Can you think of
+# any problems that this may cause for analysis? What could you do to remedy these
+# problems? Try them and report on whether they worked.
+
+
+# Create a time-series plot of these tweet count variables in a way that you think is
+# meaningful. Explain what considerations you thought through and what the figure shows.
+
+ggplot(tweets, aes(x = Date, y = Count, color = Tweets)) + 
+  geom_line() +
+  theme_minimal()
+
 
 
 
